1f2280ee9b485594dee4ded8ff05f9ab76613e27
Josh <imperviousmc@gmail.com>
2020-08-25 13:39:30 -0400

    it works now possibly

--- src/main/java/com/gitlab/impervious/Main.java
+++ src/main/java/com/gitlab/impervious/Main.java
@@ -16,7 +16,6 @@ import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Guild;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
